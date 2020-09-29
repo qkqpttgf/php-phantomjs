4652300828088e15a4609590d632b150d06ff3a3
qkqpttgf <45693631+qkqpttgf@users.noreply.github.com>
2020-09-29 18:40:47 +0800

    Update index.php

--- index.php
+++ index.php
@@ -29,6 +29,16 @@ function h2p($url, $jpg)
     $client->send($request, $response);
 }
 
+function base64EncodeImage($image_file)
+{
+  $base64_image = '';
+  $image_info = getimagesize($image_file);
+  $image_data = fread(fopen($image_file, 'r'), filesize($image_file));
+  //$base64_image = 'data:' . $image_info['mime'] . ';base64,' . chunk_split(base64_encode($image_data));
+  $base64_image = 'data:' . $image_info['mime'] . ';base64,' . base64_encode($image_data);
+  return $base64_image;
+}
+
     $url = $_POST['aimurl'];
     $tmp = sys_get_temp_dir();
     $jpg = $tmp . '/tmp.jpg';
@@ -41,5 +51,5 @@ function h2p($url, $jpg)
     <input type="text" name="aimurl"></input>
     <input type="submit"></input>
 </form>
-    <img src="<?php if ($url!='') echo $jpg;?>"></img>
+<img src="<?php if ($url!='') echo base64EncodeImage($jpg);?>"></img>
     
