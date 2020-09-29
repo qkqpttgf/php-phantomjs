656012fdd5c7f96385c0521687e270e63565b021
qkqpttgf <45693631+qkqpttgf@users.noreply.github.com>
2020-09-29 18:38:02 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
-require 'vendor/autoload.php';
+
+function h2p($url, $jpg)
+{
+    require 'vendor/autoload.php';
     use JonnyW\PhantomJs\Client;
 
     $client = Client::getInstance();
@@ -8,12 +11,12 @@ require 'vendor/autoload.php';
     $height = 600;
     $top    = 0;
     $left   = 0;
-    
+
     /** 
      * @see JonnyW\PhantomJs\Http\CaptureRequest
      **/
-    $request = $client->getMessageFactory()->createCaptureRequest('http://baidu.com', 'GET');
-    $request->setOutputFile('baidu.jpg');
+    $request = $client->getMessageFactory()->createCaptureRequest($url, 'GET');
+    $request->setOutputFile($jpg);
     $request->setViewportSize($width, $height);
     $request->setCaptureDimensions($width, $height, $top, $left);
 
@@ -24,7 +27,19 @@ require 'vendor/autoload.php';
 
     // Send the request
     $client->send($request, $response);
-    
-    ?>
-    <img src="baidu.jpg"></img>
+}
+
+    $url = $_POST['aimurl'];
+    $tmp = sys_get_temp_dir();
+    $jpg = $tmp . '/tmp.jpg';
+    unlink($base);
+    if ($url!='') {
+        h2p($url, $jpg);
+    }
+?>
+<form method="post">
+    <input type="text" name="aimurl"></input>
+    <input type="submit"></input>
+</form>
+    <img src="<?php if ($url!='') echo $jpg;?>"></img>
     
